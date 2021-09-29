cc86db744f518413e9d0df4f83cf0bab75f17df4
Lipeng Zhang <lipeng.zhang@grabtaxi.com>
2021-09-29 11:53:53 +0800

    目录存在时, 直接更新代码

--- imports/setup.sh
+++ imports/setup.sh
@@ -7,15 +7,28 @@ fi
 
 work_dir="$GOPATH/src"
 git_path="github.com/zhanglp92/plugins"
+git_dir="$work_dir/$git_path"
 
-cd $work_dir
-git clone -- https://${git_path}.git $git_path
-if [ $? != 0 ]; then
-  echo "ERR: clone fail"
-  exit 1
+
+if [ -e "$git_dir" ]; then
+  cd $git_dir
+  git pull
+  if [ $? != 0 ]; then
+    echo "ERR: git pull failed, src: $git_dir"
+    exit 1
+  fi
+else
+  cd $work_dir
+  git clone -- https://${git_path}.git $git_path
+  if [ $? != 0 ]; then
+    echo "ERR: clone fail"
+    exit 1
+  fi
 fi
 
-cd $git_path/imports
+cd $git_dir/imports
 GO111MODULE=on go install
 
 echo "done."
+
+echo "bin: $GOPATH/bin/imports"
